f89cdb81b028af76c2a0e78c9cfcf31b4ef4e84b
ComputerLab <roccoz2004@gmail.com>
2022-02-22 09:31:44 -0800

    Refactored code

--- src/dev/rocco/activity2/SongsFromFiles.java
+++ src/dev/rocco/activity2/SongsFromFiles.java
@@ -1,29 +1,23 @@
 package dev.rocco.activity2;
+
 import java.util.*;
 import java.io.*;
 
-public class SongsFromFiles
-{
-    public static void main(String[]args)
-    {
-        try
-        {
+public class SongsFromFiles {
+    public static void main(String[] args) {
+        try {
             Scanner input = new Scanner(new File("src/dev/rocco/activity2/mySongs.txt"));
             ArrayList<Song> mySongs = new ArrayList<>();
-            while(input.hasNext())
-            {
+            while (input.hasNext()) {
                 String t = input.nextLine();
                 String a = input.nextLine();
                 String m = input.nextLine();
                 String g = input.nextLine();
-                Song mySong = new Song(t,a,g,Double.parseDouble(m));
+                Song mySong = new Song(t, a, g, Double.parseDouble(m));
                 mySongs.add(mySong);
-
+                mySong.play();
             }
-
-        }
-        catch(IOException err)
-        {
+        } catch (IOException err) {
             System.out.println("File Not Found.");
         }
     }
