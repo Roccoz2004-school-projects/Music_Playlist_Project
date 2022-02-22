551de46dc6949896e419de914a295089b65995f3
ComputerLab <roccoz2004@gmail.com>
2022-02-22 09:31:44 -0800

    Fixed play method of class Song. Added play method

--- src/dev/rocco/activity2/Song.java
+++ src/dev/rocco/activity2/Song.java
@@ -6,8 +6,7 @@ package dev.rocco.activity2;
  * @author Mr. B
  * @version (a version number or a date)
  */
-public class Song
-{
+public class Song {
     // instance variables - replace the example below with your own
     private String title;
     private String artist;
@@ -17,8 +16,7 @@ public class Song
     /**
      * Constructor for objects of class Song
      */
-    public Song()
-    {
+    public Song() {
         // initialise instance variables
         title = "Chopsticks";
         artist = "anonymous";
@@ -26,25 +24,23 @@ public class Song
         time = 0.0;
     }
 
-    public Song(String t, String a,String g, double m)
-    {
+    public Song(String t, String a, String g, double m) {
         // initialise instance variables
         title = t;
         artist = a;
         genre = g;
         time = m;
     }
-    public String getGenre()
-    {
+
+    public String getGenre() {
         return genre;
     }
-    public double getTime()
-    {
+
+    public double getTime() {
         return time;
     }
 
-    public void play()
-    {
-        System.out.println("Title: "+ title);
+    public void play() {
+        System.out.println("Title: " + title);
     }
 }
