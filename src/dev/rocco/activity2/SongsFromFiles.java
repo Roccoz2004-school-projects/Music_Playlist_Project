cc4aef96894118d4751098910a95153fe2d46964
Roccoz2004 <roccoz2004@gmail.com>
2022-02-23 00:41:41 -0800

    WIP - Added integer input for mySong method Experimenting with array list from .txt file.

--- src/dev/rocco/activity2/SongsFromFiles.java
+++ src/dev/rocco/activity2/SongsFromFiles.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class SongsFromFiles {
+
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("src/dev/rocco/activity2/mySongs.txt"));
@@ -17,8 +18,11 @@ public class SongsFromFiles {
                 mySongs.add(mySong);
                 mySong.play();
             }
+            System.out.println(mySongs.get(0));
+
         } catch (IOException err) {
             System.out.println("File Not Found.");
         }
+
     }
 }
