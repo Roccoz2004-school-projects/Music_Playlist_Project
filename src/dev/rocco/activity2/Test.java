09429e9c4fb36e607eabfcf735e5d1aed72e9a53
ComputerLab <roccoz2004@gmail.com>
2022-02-24 09:32:11 -0800

    New code Updated assignment

--- src/dev/rocco/activity2/Test.java
+++ src/dev/rocco/activity2/Test.java
@@ -1,24 +1,28 @@
 package dev.rocco.activity2;
 
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class SongsFromFiles {
+public class Test {
 
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("src/dev/rocco/activity2/mySongs.txt"));
             ArrayList<Song> mySongs = new ArrayList<>();
+            Song mySong = null;
+
             while (input.hasNext()) {
                 String t = input.nextLine();
                 String a = input.nextLine();
                 String m = input.nextLine();
                 String g = input.nextLine();
-                Song mySong = new Song(t, a, g, Double.parseDouble(m));
-                mySong.play();
-                mySongs.add(mySong);
+                mySong = new Song(t, a, g, Double.parseDouble(m));
+                if (g.equals("classical")) {
+                    mySong.play();
+                }
             }
-
 
         } catch (IOException err) {
             System.out.println("File Not Found.");
